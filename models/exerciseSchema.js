8f17950f912c5e0a94420bf5e8753d73bb115d4d
ReiTony <reimerstony@gmail.com>
2024-02-22 01:28:34 +0800

    Update port number in index.js and add exerciseSchema.js

---
+++ models/exerciseSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
