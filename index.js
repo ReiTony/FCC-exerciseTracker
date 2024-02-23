1be932b46b3db5648dfd140325b35b43ef40e85c
ReiTony <reimerstony@gmail.com>
2024-02-24 02:47:09 +0800

    Add Log model and update user and exercise routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./db/connect");
 const Exercise = require("./models/exerciseSchema");
 const User = require("./models/userSchema");
+const Log = require("./models/logSchema");
 require("dotenv").config();
 
 app.use(cors());
@@ -42,7 +43,15 @@ app.post("/api/users", async (req, res) => {
       username,
     });
 
+    const newLog = new Log({
+      username,
+      count: 1,
+      userId: newUser._id,
+      log: [],
+    });
+
     await newUser.save();
+    await newLog.save();
     return res.json({ username: newUser.username, _id: newUser._id });
   } catch (error) {
     console.error(error);
@@ -77,10 +86,35 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
       description,
       duration,
       date: date ? new Date(date) : Date.now(),
-      user: user._id,
+      userId: user._id,
     });
 
     await newExercise.save();
+
+    const userLog = await Log.findOne({ userId: _id });
+    if (userLog) {
+      userLog.log.push({
+        description: newExercise.description,
+        duration: newExercise.duration,
+        date: newExercise.date,
+      });
+      userLog.count += 1;
+      await userLog.save();
+    } else {
+      const newUserLog = new Log({
+        username: user.username,
+        count: 1,
+        userId: user._id,
+        log: [
+          {
+            description: newExercise.description,
+            duration: newExercise.duration,
+            date: newExercise.date,
+          },
+        ],
+      });
+      await newUserLog.save();
+    }
     return res.json({
       username: user.username,
       description: newExercise.description,
@@ -88,7 +122,62 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
       _id: user._id,
       date: newExercise.date.toDateString(),
     });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: error.message || "Internal Server Error" });
+  }
+});
 
+app.get("/api/users/:_id/logs", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const { from, to, limit } = req.query;
+
+    if (!_id) {
+      return res.status(400).json({ error: "UserID is required!" });
+    }
+
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(400).json({ error: "User not found!" });
+    }
+
+    const userLog = await Log.findOne({ userId: user._id });
+    if (!userLog) {
+      return res.json({
+        _id: user._id,
+        username: user.username,
+        count: 0,
+        log: [],
+      });
+    }
+
+    let log = userLog.log.map((exercise) => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString(),
+    }));
+
+    if (from) {
+      log = log.filter((exercise) => new Date(exercise.date) >= new Date(from));
+    }
+
+    if (to) {
+      log = log.filter((exercise) => new Date(exercise.date) <= new Date(to));
+    }
+
+    if (limit) {
+      log = log.slice(0, parseInt(limit));
+    }
+
+    return res.json({
+      _id: userLog.userId,
+      username: userLog.username,
+      count: userLog.count,
+      log,
+    });
   } catch (error) {
     console.error(error);
     return res
